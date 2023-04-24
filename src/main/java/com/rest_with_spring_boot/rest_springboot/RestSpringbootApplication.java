597c82253ff3f64e0447a381e20d23102d8893d2
Davi Noronha <davinoronha.g@gmail.com>
2023-04-24 15:56:50 -0400

    Buildando SpringBoot

---
+++ src/main/java/com/rest_with_spring_boot/rest_springboot/RestSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.rest_with_spring_boot.rest_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringbootApplication.class, args);
+	}
+
+}
